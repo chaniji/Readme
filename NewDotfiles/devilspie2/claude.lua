23c171ee875793b9459102f15e04d525de137882
chaniji <chaniji19@gmail.com>
2026-07-31 16:46:59 +0530

    Setup 2.1

--- NewDotfiles/devilspie2/claude.lua
+++ NewDotfiles/devilspie2/claude.lua
@@ -1,5 +1,5 @@
 if get_window_name() == "Claude" then
-	set_window_position(1085, 190)
-	set_window_size(680, 780)
-	set_window_geometry(1085, 190, 680, 780)
+	set_window_position(1225, 245)
+	set_window_size(655, 780)
+	set_window_geometry(1225, 245, 655, 780)
 end
---
+++ NewDotfiles/devilspie2/zen.lua
@@ -0,0 +1,5 @@
+if string.find(get_window_name(), "Zen Browser") then
+	set_window_position(35, 130)
+	set_window_size(1180, 890)
+	set_window_geometry(35, 130, 1180, 890)
+end
